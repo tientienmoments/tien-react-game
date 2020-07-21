0f8bf5385b1da10a0cbc5e5b7ad8bc424e79d5c1
tientienmoments <tientienmoments@gmail.com>
2020-07-21 15:26:59 +0700

    tien complete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
 import ChoiceCard from './components/ChoiceCard'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const choices = {
-  rock:"https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
-  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
-  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
-};
+import { CHOICES, getRoundOutcome } from "./utils";
+import ChoiceButtons from "./components/ChoiceButtons";
+import './App.css';
 
 function App() {
+  const [prompt, setGamePrompt] = useState("Start");
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [previousWinner, setPreviousWinner] = useState(null);
+  const [gameHistory, setGameHistory] = useState([]);
+
+
+  const onPlayerChoose = playerChoice => {
+    const [result, compChoice] = getRoundOutcome(playerChoice);
+    const newUserChoice = CHOICES[playerChoice];
+    const newComputerChoice = CHOICES[compChoice];
+    setPlayerChoice(newUserChoice);
+    setComputerChoice(newComputerChoice);
+    if (result === "Victory!") {
+      setPreviousWinner("You");
+    } else if (result === "Defeat!") {
+      setPreviousWinner("Computer");
+    } else {
+      setPreviousWinner("Tie");
+    }
+    setGamePrompt(result);
+
+    gameHistory.push(result);   //???Push the result of each round into our gameHistory array.
+
+    setGameHistory(gameHistory);
+  };
+
+
   return (
     <div className="App">
-  <ChoiceCard 
-  title="You" 
-  winner={false} 
-  imgURL={choices.rock} />
-  <ChoiceCard 
-  title="Computer" 
-  winner={true} 
-  imgURL={choices.paper} />
-</div>
+      <div className="container">
+        <div className="row mb-3">
+          <div className="col-md-8 themed-grid-col">
+            <ChoiceCard
+              title="Computer"
+              previousWinner={previousWinner}
+              imgURL={computerChoice && computerChoice.url}
+            />
+            <h1>{prompt}</h1>
+            <ChoiceButtons onPlayerChoose={onPlayerChoose} />
+
+            <ChoiceCard
+              title="You"
+              previousWinner={previousWinner}
+              imgURL={playerChoice && playerChoice.url}
+            />
+          </div>
+          <div className="col-md-4 themed-grid-col">
+            <h3>History</h3>
+            <ul>
+              {gameHistory.map(result => {
+                return <li>{result}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
   );
 }
 
--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -1,14 +1,34 @@
 import React from 'react'
-
-export default function ChoiceCard ( props) {   //B capital means components  //syntax inline style ben duoi style={{"width":"400px"}}
-console.log("Props:", props);  
+const DEFAULT_IMG =
+  "http://www.thewateringhole.co.uk/wp-content/uploads/2012/12/play.png"; 
+  
+export default function ChoiceCard(props) {
+    const won = props.title === props.previousWinner;
+    let className;
+    const hasPreviousGame =
+      props.previousWinner === "Computer" || props.previousWinner === "You";
+    if (hasPreviousGame) {
+      className = won ? "winner" : "loser";
+    }
+  
+    let prompt;
+    if (won) {
+      prompt = "Won";
+      className = won ? "winner" : "loser";
+    } else if (props.previousWinner === "Tie") {
+      prompt = "Tie";
+    } else if (props.previousWinner === null) {
+      prompt = "Start";
+    } else {
+      prompt = "Lose";
+    }
+  
     return (
-        <div className={`choice-card ${props.winner ? 'winner' : 'loser'}`}>
-            <h1>{props.title}</h1>
-            <img src={props.imgURL} alt={props.title}/>
-            <h3>{props.winner ? 'Won' : 'Loss'}</h3>
-        </div>
-    )
-}
-
+      <div className={`choice-card ${className}`}>
+        <h1>{props.title}</h1>
+        <img src={props.imgURL || DEFAULT_IMG} alt={props.title} />
+        <h3>{prompt}</h3>
+      </div>
+    );
+  }
 
